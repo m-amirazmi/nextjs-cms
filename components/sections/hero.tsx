06b665a756708c2a15b5c8b6b6c5188e3fcedac6
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2024-03-31 01:26:07 +0800

    Add section schema types

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,5 +1,17 @@
-import { HeroProps } from "@/lib/types";
+import { HeroProps, SectionSchema } from "@/lib/types";
 
 export default function Hero({ title = "Default Hero" }: HeroProps) {
   return <div>{title}</div>;
 }
+
+export const heroSchema: SectionSchema = {
+  name: "hero",
+  settings: [
+    {
+      id: "hero-title",
+      label: "Hero Title",
+      type: "radio",
+      options: [{ label: "sd", value: 232 }],
+    },
+  ],
+};
--- lib/types.ts
+++ lib/types.ts
@@ -33,3 +33,115 @@ export interface TitleDescriptionProps {
 export interface SectionsDropdownProps {
   handleCloseDropdown: () => void;
 }
+
+// SECTION SCHEMA TYPES
+export type SectionSchemaType =
+  | "text"
+  | "textarea"
+  | "range"
+  | "checkbox"
+  | "number"
+  | "radio"
+  | "select";
+
+export interface BaseSectionSchemaSetting {
+  id: string;
+  label: string;
+}
+
+type BaseSettingMap = {
+  [key in SectionSchemaType]: BaseSectionSchemaSetting & { type: key };
+};
+
+export interface SelectionOptionSectionSchemaSetting {
+  value: string | number;
+  label: string;
+}
+
+type OptionalSettingsMap = {
+  range: {
+    min: number;
+    max: number;
+    step?: number;
+    default?: number;
+    unit?: string;
+  };
+  text: {
+    default?: string;
+  };
+  textarea: {
+    default?: string;
+  };
+  checkbox: {
+    default?: boolean;
+  };
+  number: {
+    default?: boolean;
+  };
+  radio: {
+    options: SelectionOptionSectionSchemaSetting[];
+    default?: string;
+  };
+  select: {
+    options: SelectionOptionSectionSchemaSetting[];
+    default?: string;
+  };
+};
+
+type MergedSetting<T extends SectionSchemaType> = BaseSettingMap[T] &
+  OptionalSettingsMap[T];
+
+export type SectionSchemaSetting = {
+  [Key in SectionSchemaType]: MergedSetting<Key>;
+}[SectionSchemaType];
+
+export interface SectionSchema {
+  name: string;
+  settings: SectionSchemaSetting[];
+}
+
+// export interface RangeSectionSchemaSetting extends BaseSectionSchemaSetting {
+//   type: "range";
+//   min: number;
+//   max: number;
+//   step?: number;
+//   default?: number;
+//   unit?: string;
+// }
+
+// export interface TextSectionSchemaSetting extends BaseSectionSchemaSetting {
+//   type: "text";
+//   default?: string;
+// }
+// export interface TextAreaSectionSchemaSetting extends BaseSectionSchemaSetting {
+//   type: "textarea";
+//   default?: string;
+// }
+// export interface CheckboxSectionSchemaSetting extends BaseSectionSchemaSetting {
+//   type: "checkbox";
+//   default?: boolean;
+// }
+// export interface NumberSectionSchemaSetting extends BaseSectionSchemaSetting {
+//   type: "number";
+//   default?: number;
+// }
+
+// export interface RadioSectionSchemaSetting extends BaseSectionSchemaSetting {
+//   type: "radio";
+//   options: SelectionOptionSectionSchemaSetting[];
+//   default?: string;
+// }
+// export interface SelectSectionSchemaSetting extends BaseSectionSchemaSetting {
+//   type: "select";
+//   options: SelectionOptionSectionSchemaSetting[];
+//   default?: string;
+// }
+
+// export type SectionSchemaSetting =
+//   | RangeSectionSchemaSetting
+//   | TextSectionSchemaSetting
+//   | TextAreaSectionSchemaSetting
+//   | CheckboxSectionSchemaSetting
+//   | NumberSectionSchemaSetting
+//   | RadioSectionSchemaSetting
+//   | SelectSectionSchemaSetting;
