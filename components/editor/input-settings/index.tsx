9440e526099e2f871bee33b0db0116b51bd44cb3
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2024-04-04 02:05:54 +0800

    Setting up input settings file

--- components/editor/input-settings/index.tsx
+++ components/editor/input-settings/index.tsx
@@ -1,15 +1,39 @@
+import CheckboxInput from "./checkbox-input";
+import ColorPicker from "./color-picker";
+import ImagePicker from "./image-picker";
+import NumberInput from "./number-input";
+import RadioInput from "./radio-input";
+import RangeInput from "./range-input";
+import RichtextInput from "./richtext-input";
+import SelectInput from "./select-input";
 import TextInput from "./text-input";
+import TextareaInput from "./textarea-input";
+import UrlInput from "./url-input";
+import VideoInput from "./video-input";
 
 interface InputSettingsProps {
-  name: string;
+  settings: {
+    type: string;
+  }[];
 }
 
 interface InputSettingsObj {
-  [key: string]: React.ReactNode;
+  [key: string]: React.FC<any>;
 }
 
 const inputSettings: InputSettingsObj = {
   text: TextInput,
+  textarea: TextareaInput,
+  checkbox: CheckboxInput,
+  color: ColorPicker,
+  image: ImagePicker,
+  number: NumberInput,
+  radio: RadioInput,
+  range: RangeInput,
+  richtext: RichtextInput,
+  select: SelectInput,
+  url: UrlInput,
+  video: VideoInput,
 };
 
 export default function InputSettings({ name }: InputSettingsProps) {
