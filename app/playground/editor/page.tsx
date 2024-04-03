33320e10bc9a0880e3fa35524576403920b17896
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2024-04-04 02:01:39 +0800

    Push all

--- app/playground/editor/page.tsx
+++ app/playground/editor/page.tsx
@@ -1,3 +1,23 @@
+import TextInput from "@/components/editor/input-settings/text-input";
+
 export default function PlaygroundEditor() {
-  return <></>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h2 className="text-xl">Input Settings</h2>
+      <div className="flex flex-col gap-y-2">
+        <TextInput />
+        <p>TextArea Input</p>
+        <p>Checkbox Input</p>
+        <p>Number Input</p>
+        <p>Radio Input</p>
+        <p>Range Input</p>
+        <p>Select Input</p>
+        <p>Color Picker</p>
+        <p>Image Picker</p>
+        <p>URL Input</p>
+        <p>Video</p>
+        <p>RichText Input</p>
+      </div>
+    </div>
+  );
 }
